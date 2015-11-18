0c5c9269559f90ca32b1cadfcd0743af5a27582e
Walter Moore <walteragnewmoore@gmail.com>
2015-11-18 13:56:07 -0600

    added alerts for empty input fields

--- Calc.js
+++ Calc.js
@@ -10,20 +10,37 @@ app.controller('CalcController', ['$scope', '$window', function ($scope, $window
 	$scope.input2=null;
 	
 		$scope.addNum = function(){
+			if ($scope.input1 == null || $scope.input2 == null){
+				alert("Both operands are required");
+				return;
+			}else{
 			$scope.result = $scope.input1 + $scope.input2;
+			};
 		}
 		
 		$scope.subtractNum = function(){
+			if ($scope.input1 == null || $scope.input2 == null){
+				alert("Both operands are required");
+				return;
+			}else{
 			$scope.result = $scope.input1 - $scope.input2;
+			};
 		}	
 	
 		$scope.multiplyNum = function(){
+			if ($scope.input1 == null || $scope.input2 == null){
+				alert("Both operands are required");
+				return;
+			}else{
 			$scope.result = $scope.input1 * $scope.input2;
+			};
 		}
 		
 		$scope.divideNum = function(){
-			if ($scope.input2 == 0){
-				
+			if ($scope.input1 == null || $scope.input2 == null){
+				alert("Both operands are required");
+				return;
+			}else if ($scope.input2 == 0){
 				alert("Are you trying to kill us all? Please don't divide by zero");
 				return;
 			}else{
